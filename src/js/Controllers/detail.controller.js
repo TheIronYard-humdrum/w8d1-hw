45feaf37c4e70cf5907a0a529d64fa8287562534
humdrum <jjrajani@gmail.com>
2016-07-26 09:14:04 -0400

    working on edit feature

--- src/js/Controllers/detail.controller.js
+++ src/js/Controllers/detail.controller.js
@@ -1,12 +1,42 @@
-function DetailController (ContactService, $stateParams) {
+function DetailController (ContactService, $stateParams, $state, $scope) {
+
   let vm = this;
+
+  this.edit = false;
+  vm.editMode = editMode;
+  vm.isEditMode = isEditMode;
+  vm.deleteCon = deleteCon;
+  vm.submitEdit = submitEdit;
+
   init();
 
+  function deleteCon (contact, $scope) {
+    ContactService.deleteContact(contact).then( (res) => {
+      $state.go('root.home')
+    });
+  }
+
+  function submitEdit() {
+    $scope.new.newName !== undefined ? vm.contact.name = $scope.new.newName : vm.contact.name = vm.contact.name;
+    console.log(vm.contact);
+  }
+
+  function editMode() {
+    this.edit = !this.edit;
+  }
+
+  function isEditMode() {
+    return !this.edit;
+  }
+
   function init() {
+    console.log(vm.deleteCon)
     ContactService.getContact($stateParams.contact).then ( (res) => {
       vm.contact = res.data
     });
+    console.log(vm.contact)
   }
+
 }
-DetailController.$inject = ['ContactService', '$stateParams'];
+DetailController.$inject = ['ContactService', '$stateParams', '$state', '$scope'];
 export { DetailController };
